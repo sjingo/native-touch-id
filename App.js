f6080b3a5d0d010d2b3befadd9ede81188c06109
Stef <sjingo@users.noreply.github.com>
2018-12-02 12:45:27 +0000

    biometric login

--- App.js
+++ App.js
@@ -1,11 +1,106 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+  Alert,
+  Image,
+} from 'react-native';
+import Expo, { Constants } from 'expo';
+import DropdownAlert from 'react-native-dropdownalert';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    compatible: false,
+  };
+
+  componentDidMount() {
+    this.checkDeviceForHardware();
+  }
+
+  checkDeviceForHardware = async () => {
+    let compatible = await Expo.Fingerprint.hasHardwareAsync();
+    this.setState({ compatible });
+    if (!compatible) {
+      this.showIncompatibleAlert();
+    }
+  };
+
+  showIncompatibleAlert = () => {
+    this.dropdown.alertWithType(
+      'error',
+      'Incompatible Device',
+      'Current device does not have the necessary hardware to use this API.'
+    );
+  };
+
+  checkForBiometrics = async () => {
+    let biometricRecords = await Expo.Fingerprint.isEnrolledAsync();
+    if (!biometricRecords) {
+      this.dropdown.alertWithType(
+        'warn',
+        'No Biometrics Found',
+        'Please ensure you have set up biometrics in your OS settings.'
+      );
+    } else {
+      this.handleLoginPress();
+    }
+  };
+
+  handleLoginPress = () => {
+    if (Platform.OS === 'android') {
+      this.showAndroidAlert();
+    } else {
+      this.scanBiometrics();
+    }
+  };
+
+  showAndroidAlert = () => {
+    Alert.alert('Fingerprint Scan', 'Place your finger over the touch sensor.');
+    this.scanBiometrics();
+  };
+
+  scanBiometrics = async () => {
+    let result = await Expo.Fingerprint.authenticateAsync('Biometric Scan.');
+    if (result.success) {
+      this.dropdown.alertWithType(
+        'success',
+        'You are you!',
+        'Bio-Authentication succeeded.'
+      );
+    } else {
+      this.dropdown.alertWithType(
+        'error',
+        'Uh oh!',
+        'Bio-Authentication failed or canceled.'
+      );
+    }
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Image
+          style={styles.logo}
+          source={require('./assets/icon.png')}
+        />
+        <TouchableOpacity
+          onPress={
+            this.state.compatible
+              ? this.checkForBiometrics
+              : this.showIncompatibleAlert
+          }
+          style={styles.button}>
+          <Text style={styles.buttonText}>
+            Bio Login
+          </Text>
+        </TouchableOpacity>
+        <DropdownAlert
+          ref={ref => (this.dropdown = ref)}
+          closeInterval={5000}
+        />
       </View>
     );
   }
@@ -14,8 +109,30 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#056ecf',
+  },
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
+    width: 200,
+    height: 60,
+    backgroundColor: 'transparent',
+    borderRadius: 5,
+    borderWidth: 1.5,
+    borderColor: '#fff',
+  },
+  buttonText: {
+    fontSize: 30,
+    color: '#fff',
+    textShadowColor: 'rgba(0,0,0,0.30)',
+    textShadowOffset: { width: 0, height: 1 },
+    textShadowRadius: 2,
+  },
+  logo: {
+    height: 128,
+    width: 128,
   },
 });
